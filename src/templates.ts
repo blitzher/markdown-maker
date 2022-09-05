936865fae29582e35cea573c7e931b972308460f
blitzher <skovbooorg@gmail.com>
2022-09-05 10:14:39 +0200

    added mathjax template

--- src/templates.ts
+++ src/templates.ts
@@ -6,6 +6,8 @@ function new_template(name: string, content: string) {
 
 /* initialize default templates */
 const presentation_template = require("../src/templates/presentation.js");
+const mathjax_template = require("../src/templates/mathjax.js");
 new_template("presentation", presentation_template);
+new_template("mathjax", mathjax_template);
 
 export default templates;
